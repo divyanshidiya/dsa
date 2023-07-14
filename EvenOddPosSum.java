db1efe057c5b1ef7b6eb78f6a0be46eb8879c192
Divyanshi Chandra <93268027+divyanshidiya@users.noreply.github.com>
2023-07-14 23:05:13 +0530

    Add files via upload

---
+++ EvenOddPosSum.java
@@ -0,0 +1,13 @@
+public class EvenOddPosSum {
+    static int[]sumEvenOddPos(int num, int pos){
+        if(num==0){
+            int result[]=new int[2];
+            return result;
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
